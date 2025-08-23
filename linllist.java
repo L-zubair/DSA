df20868ee7ff07c1a5cc27445872c3b29762aab0
l-zubair <csecode124@gmail.com>
2025-08-23 13:15:38 +0530

    Add singly linked list implementation

---
+++ linllist.java
@@ -0,0 +1,31 @@
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+    public class linllist {
+    public static void main(String[] args) {
+    //creatting node
+    Node head = new Node(10);
+    Node second = new Node(20);
+    Node third = new Node(30);
+    //linking
+    head.next=second;
+    second.next=third;
+    //Traverse and print 
+    Node current=head;
+    while (current != null) {
+        System.out.println(current.data+"->");
+        current=current.next;
+        
+    }
+    System.out.println("null");
+}
+        
+    }
+
