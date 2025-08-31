071f790a45bde4df1ece90d25cf57c928ad5af54
l-zubair <csecode124@gmail.com>
2025-08-31 21:31:01 +0530

    Add LongestCommonPrefix

---
+++ LongestCommonprefix.java
@@ -0,0 +1,20 @@
+class LongestCommonprefix{
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        String prefix = strs[0]; // assume first string is prefix
+        
+        for (int i = 1; i < strs.length; i++) {
+            // while current string does not start with prefix
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
