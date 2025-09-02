ca90efa5ae7648d45a3be075de581ddf6a6595f0
l-zubair <csecode124@gmail.com>
2025-09-02 20:09:01 +0530

    adding the mergeTwoLists

---
+++ MergeTwoLists.java
@@ -0,0 +1,52 @@
+public class MergeTwoLists {
+    
+    // Definition for singly-linked list
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Step 1: Create a dummy node
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        // Step 2: Compare and attach nodes
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next; // Move tail
+        }
+
+        // Step 3: Attach remaining nodes
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Example: list1 = [1,2,4], list2 = [1,3,4]
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode mergedList = mergeTwoLists(list1, list2);
+
+        // Print merged list
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
+    }
+}
